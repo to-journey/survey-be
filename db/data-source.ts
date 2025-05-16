9131943d1fce7e0e16bd1db00e47670597a45191
to-journey <bs9961030@gmail.com>
2025-05-16 13:35:24 +0900

    feat: update vercel config

--- db/data-source.ts
+++ db/data-source.ts
@@ -6,7 +6,7 @@ import 'dotenv/config';
 export const config = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  port: parseInt(process.env.DB_PORT || '6543', 10),
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_DATABASE || 'survey',
